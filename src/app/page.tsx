50094f8ef37dc879dc90ab742f9669efdf084de7
Jacob Snarr <jacobsnarr@gmail.com>
2023-09-24 04:06:22 -0400

    Add camera controls and routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,24 @@
 import Button from '@/components/Button';
 import MonsterPreview from '@/components/MonsterPreview';
 import Navbar from '@/components/Navbar';
-import { useEffect, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import Webcam from "react-webcam";
 import Map from "@/components/Map"
+import CameraControls from '@/components/CameraControls';
 
 
 export default function Home() {
+
+  const [location, setLocation] = useState<Partial<GeolocationCoordinates>|null>()
+  const [currentPage, setCurrentPage] = useState("home");
+  const [facingUser, setFacingUser] = useState(false)
+
   const videoConstraints = {
     width: { min: 480 },
     height: { min: 720 },
     aspectRatio: 0.6666666667,
-    facingMode: "environment"
+    facingMode: facingUser ? "user" : "environment"
   };
-
-  const [location, setLocation] = useState<Partial<GeolocationCoordinates>|null>()
-  const [useWebcam, setUseWebcam] = useState(false)
   
   useEffect(() => {
     if('geolocation' in navigator) {
@@ -30,22 +33,24 @@ export default function Home() {
 
   const [visibleMap, setVisibleMap] = useState(true)
 
-  return (
+  if (currentPage == "home") {
+    return (
     <div className="w-full h-full flex justify-center">
-      { useWebcam ?
-          <Webcam videoConstraints={videoConstraints} 
+      <div className="bottom-5 absolute z-10">
+        <Navbar cameraOnClick={() => setCurrentPage("camera")} onClick={function () { setVisibleMap(!visibleMap)}}></Navbar>
+      </div>
+      {(visibleMap && location) ? (<Map location={location as GeolocationCoordinates}></Map>) : (<div> </div>)}
+    </div>)
+  } else if (currentPage == "camera") {
+    return (<div className="w-full h-full flex justify-center">
+      <div className="bottom-5 absolute z-10">
+        <CameraControls onCloseClick={() => {setCurrentPage("home")}} switchCamera={() => { setFacingUser(!facingUser) }}></CameraControls>
+      </div>
+      <Webcam videoConstraints={videoConstraints} 
             width={480} 
             height={720}
             className="absolute z-20 rounded-[50px]"
           />
-          :
-          <></>
-        }
-      <div className="bottom-5 absolute z-10">
-        <Navbar cameraOnClick={() => setUseWebcam(!useWebcam)} onClick={function () { setVisibleMap(!visibleMap)}}></Navbar>
-        
-      </div>
-      {(visibleMap && location) ? (<Map location={location as GeolocationCoordinates}></Map>) : (<div> </div>)}
-    </div>
-  )
+    </div>)
+  }
 }
