0f3a9dc3ce55c947b5fce4c69a46530ef14d6108
Parth099 <parthnpatel715@gmail.com>
2023-09-24 05:39:53 -0400

    fix re-render issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,97 @@
 "use client";
-import Button from "@/components/Button";
-import MonsterPreview from "@/components/MonsterPreview";
 import Navbar from "@/components/Navbar";
-import useGetUserData from "@/hooks/useGetUserData";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
 import Webcam from "react-webcam";
-import Map from "@/components/Map"
-import CameraControls from '@/components/CameraControls';
-
+import Map from "@/components/Map";
+import CameraControls from "@/components/CameraControls";
+import axios, { AxiosResponse } from "axios";
+import { RecognizeResponse } from "@/hooks/useSendRecognizeRequest";
 
 export default function Home() {
+    const [location, setLocation] = useState<Partial<GeolocationCoordinates> | null>();
+    const [currentPage, setCurrentPage] = useState("home");
+    const [facingUser, setFacingUser] = useState(false);
+    const [base64Image, setBase64Image] = useState<string | undefined>(undefined);
 
-  const [location, setLocation] = useState<Partial<GeolocationCoordinates>|null>()
-  const [currentPage, setCurrentPage] = useState("home");
-  const [facingUser, setFacingUser] = useState(false)
+    const videoConstraints = {
+        width: { min: 480 },
+        height: { min: 720 },
+        aspectRatio: 0.6666666667,
+        facingMode: facingUser ? "user" : "environment",
+    };
 
-  const videoConstraints = {
-    width: { min: 480 },
-    height: { min: 720 },
-    aspectRatio: 0.6666666667,
-    facingMode: facingUser ? "user" : "environment"
-  };
-  
-  useEffect(() => {
-    if('geolocation' in navigator) {
-        // Retrieve latitude & longitude coordinates from `navigator.geolocation` Web API
-        navigator.geolocation.getCurrentPosition(({ coords }) => {
-            const { latitude, longitude } = coords;
-            setLocation({ latitude, longitude });
-        })
-    }
-}, []);
+    useEffect(() => {
+        if ("geolocation" in navigator) {
+            // Retrieve latitude & longitude coordinates from `navigator.geolocation` Web API
+            navigator.geolocation.getCurrentPosition(({ coords }) => {
+                const { latitude, longitude } = coords;
+                setLocation({ latitude, longitude });
+            });
+        }
+    }, []);
+
+    const webcamRef = useRef<Webcam | null>(null);
+    const capture = useCallback(() => {
+        if (!webcamRef.current) return;
+        const imageSrc = webcamRef.current.getScreenshot();
+
+        if (!imageSrc) return;
+        const base64part = imageSrc.split("data:image/jpeg;base64,")[1];
+
+        if (base64part) setBase64Image(base64part);
+    }, [webcamRef]);
 
     const [visibleMap, setVisibleMap] = useState(true);
-    const [userData, triggerRefresh] = useGetUserData({
-        userId: "12ACE",
-    });
 
-    console.log(userData);
+    useEffect(() => {
+        if (!base64Image) return;
 
-  if (currentPage == "home") {
-    return (
-    <div className="w-full h-full flex justify-center">
-      <div className="bottom-5 absolute z-10">
-        <Navbar cameraOnClick={() => setCurrentPage("camera")} onClick={function () { setVisibleMap(!visibleMap)}}></Navbar>
-      </div>
-      {(visibleMap && location) ? (<Map location={location as GeolocationCoordinates}></Map>) : (<div> </div>)}
-    </div>)
-  } else if (currentPage == "camera") {
-    return (<div className="w-full h-full flex justify-center">
-      <div className="bottom-5 absolute z-10">
-        <CameraControls onCloseClick={() => {setCurrentPage("home")}} switchCamera={() => { setFacingUser(!facingUser) }}></CameraControls>
-      </div>
-      <Webcam videoConstraints={videoConstraints} 
-            width={480} 
-            height={720}
-            className="absolute z-20 rounded-[50px]"
-          />
-    </div>)
-  }
+        const BACKEND_URL = `${process.env.NEXT_PUBLIC_BACKEND_URL as string}/recognize`;
+
+        console.log("sending:", base64Image);
+        const imagePost = axios.post<any, AxiosResponse<RecognizeResponse>>(BACKEND_URL, {
+            userId: "Parth099",
+            image: base64Image,
+        });
+    }, [base64Image]);
+
+    if (currentPage == "home") {
+        return (
+            <div className="w-full h-full flex justify-center">
+                <div className="bottom-5 absolute z-10">
+                    <Navbar
+                        cameraOnClick={() => setCurrentPage("camera")}
+                        onClick={function () {
+                            setVisibleMap(!visibleMap);
+                        }}
+                    ></Navbar>
+                </div>
+                {visibleMap && location ? <Map location={location as GeolocationCoordinates}></Map> : <div> </div>}
+            </div>
+        );
+    } else if (currentPage == "camera") {
+        return (
+            <div className="w-full h-full flex justify-center">
+                <div className="bottom-5 absolute z-10">
+                    <CameraControls
+                        onCloseClick={() => {
+                            setCurrentPage("home");
+                        }}
+                        switchCamera={() => {
+                            setFacingUser(!facingUser);
+                        }}
+                        onCameraShutter={capture}
+                    ></CameraControls>
+                </div>
+                <Webcam
+                    screenshotFormat="image/jpeg"
+                    videoConstraints={videoConstraints}
+                    width={480}
+                    height={720}
+                    className="absolute z-20 rounded-[50px]"
+                    ref={webcamRef}
+                />
+            </div>
+        );
+    }
 }
--- src/components/CameraControls.tsx
+++ src/components/CameraControls.tsx
@@ -2,26 +2,40 @@ import { MouseEventHandler, useState } from "react";
 import Button from "./Button";
 import MonsterPreview from "./MonsterPreview";
 
+type CamClick = MouseEventHandler<HTMLDivElement>;
 
-export default function CameraControls({onCloseClick, switchCamera}: {onCloseClick: MouseEventHandler<HTMLDivElement>, switchCamera: MouseEventHandler<HTMLDivElement>}) {
-  return (
-    <div className="flex flex-row gap-16 p-2 rounded-[75px]">
-      <Button onClick={onCloseClick}>
-        <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M24.4583 2.45038L22.0496 0.041626L12.5 9.59121L2.95042 0.041626L0.541672 2.45038L10.0913 12L0.541672 21.5495L2.95042 23.9583L12.5 14.4087L22.0496 23.9583L24.4583 21.5495L14.9088 12L24.4583 2.45038Z" fill="#4ade80"/>
-        </svg>
-      </Button>
-      <Button>
-      <svg width="52" height="52" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect x="1.5" y="1" width="62" height="62" rx="31" fill="white" stroke="black" stroke-width="2"/>
-      </svg>
-
-      </Button>
-      <Button onClick={switchCamera}>
-        <svg width="29" height="26" viewBox="0 0 29 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M25.5 3.375H21.1413L18.625 0.625H10.375L7.85875 3.375H3.5C1.9875 3.375 0.75 4.6125 0.75 6.125V22.625C0.75 24.1375 1.9875 25.375 3.5 25.375H25.5C27.0125 25.375 28.25 24.1375 28.25 22.625V6.125C28.25 4.6125 27.0125 3.375 25.5 3.375ZM14.5 21.25C10.705 21.25 7.625 18.17 7.625 14.375H4.875L8.3125 10.9375L11.75 14.375H9C9 17.4137 11.4613 19.875 14.5 19.875C15.2975 19.875 16.0538 19.6962 16.7275 19.3937L17.745 20.4112C16.7687 20.92 15.6825 21.25 14.5 21.25ZM20.6875 17.8125L17.25 14.375H20C20 11.3363 17.5387 8.875 14.5 8.875C13.7025 8.875 12.9462 9.05375 12.2725 9.35625L11.255 8.3525C12.2313 7.83 13.3175 7.5 14.5 7.5C18.295 7.5 21.375 10.58 21.375 14.375H24.125L20.6875 17.8125Z" fill="#4ade80"/>
-        </svg>
-      </Button>
-    </div>
-  )
+export default function CameraControls({
+    onCloseClick,
+    switchCamera,
+    onCameraShutter,
+}: {
+    onCloseClick: CamClick;
+    switchCamera: CamClick;
+    onCameraShutter: CamClick;
+}) {
+    return (
+        <div className="flex flex-row gap-16 p-2 rounded-[75px]">
+            <Button onClick={onCloseClick}>
+                <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M24.4583 2.45038L22.0496 0.041626L12.5 9.59121L2.95042 0.041626L0.541672 2.45038L10.0913 12L0.541672 21.5495L2.95042 23.9583L12.5 14.4087L22.0496 23.9583L24.4583 21.5495L14.9088 12L24.4583 2.45038Z"
+                        fill="#4ade80"
+                    />
+                </svg>
+            </Button>
+            <Button onClick={onCameraShutter}>
+                <svg width="52" height="52" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect x="1.5" y="1" width="62" height="62" rx="31" fill="white" stroke="black" stroke-width="2" />
+                </svg>
+            </Button>
+            <Button onClick={switchCamera}>
+                <svg width="29" height="26" viewBox="0 0 29 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M25.5 3.375H21.1413L18.625 0.625H10.375L7.85875 3.375H3.5C1.9875 3.375 0.75 4.6125 0.75 6.125V22.625C0.75 24.1375 1.9875 25.375 3.5 25.375H25.5C27.0125 25.375 28.25 24.1375 28.25 22.625V6.125C28.25 4.6125 27.0125 3.375 25.5 3.375ZM14.5 21.25C10.705 21.25 7.625 18.17 7.625 14.375H4.875L8.3125 10.9375L11.75 14.375H9C9 17.4137 11.4613 19.875 14.5 19.875C15.2975 19.875 16.0538 19.6962 16.7275 19.3937L17.745 20.4112C16.7687 20.92 15.6825 21.25 14.5 21.25ZM20.6875 17.8125L17.25 14.375H20C20 11.3363 17.5387 8.875 14.5 8.875C13.7025 8.875 12.9462 9.05375 12.2725 9.35625L11.255 8.3525C12.2313 7.83 13.3175 7.5 14.5 7.5C18.295 7.5 21.375 10.58 21.375 14.375H24.125L20.6875 17.8125Z"
+                        fill="#4ade80"
+                    />
+                </svg>
+            </Button>
+        </div>
+    );
 }
